2216a97d409e851137d38430b284b77ba55dba04
Utkarsh Shukla <utk.shukla@gmail.com>
2023-03-20 19:16:38 +0530

    not working react-umg

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,6 @@
 /// <reference path="cpp/index.d.ts" />
 /// <reference path="ffi/index.d.ts" />
 /// <reference path="puerts/index.d.ts" />
+/// <reference path="react-umg/index.d.ts" />
+/// <reference path="node_modules/@types/react/index.d.ts" />
+/// <reference path="node_modules/@types/node/index.d.ts" />
